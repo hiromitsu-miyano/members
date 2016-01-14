2753e78734c2ab98001bec321a9b1d38bd4518db
admin <hiromitsu_miyano@kunisys.co.jp>
2016-01-14 09:01:40 +0900

    画像ファイル追加

--- src/main/java/jp/co/kunisys/member/config/SecurityConfig.java
+++ src/main/java/jp/co/kunisys/member/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/css/**").permitAll()
+				.antMatchers("/css/**","/images/**").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
